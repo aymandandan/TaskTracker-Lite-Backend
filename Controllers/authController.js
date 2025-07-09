fff833e5095f46780a173542cb104e1fac06fb39
AymanAD <aymandandan24680@gmail.com>
2025-07-09 11:53:54 +0300

    configured domain

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -20,7 +20,8 @@ const sendToken = (user, statusCode, res) => {
     expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: 'none',
+    domain: process.env.NODE_ENV === 'production' ? process.env.DOMAIN : 'localhost',
     path: '/',
   };
 
